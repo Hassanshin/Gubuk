b45e76d929205127b3cf37ca99ebec73f39db814
Hassanshin <f3fahmi@gmail.com>
2019-08-02 14:15:11 +0700

    concept

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform antrianParent;
+    public List<Transform> antrianList;
+
+    private void Start()
+    {
+        foreach (Transform child in antrianParent.transform)
+        {
+            antrianList.Add(child);
+            print("Foreach loop: " + child.name);
+        }
+            
+        
+    }
+}
---
+++ Assets/Scripts/Pelanggan.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pelanggan : MonoBehaviour
+{
+    private NavMeshAgent _navAgent;
+
+    void getKomponen()
+    {
+        _navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        getKomponen();
+
+
+    }
+
+    
+}
